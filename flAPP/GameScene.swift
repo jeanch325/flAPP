4354f86fb108d7bd5d699c8cb85a7c7204764a47
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-13 13:20:57 -0500

    added comments so jean knew what functions we had

--- flAPP/GameScene.swift
+++ flAPP/GameScene.swift
@@ -14,7 +14,7 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView) { // wowza that is there JEANNNNNNNNNNNNNNNNNNN
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
@@ -62,7 +62,7 @@ class GameScene: SKScene {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { // JEANNNNNNNNNNNNNNNNNNN
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
@@ -70,7 +70,7 @@ class GameScene: SKScene {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { //JEANNNNNNNNNNNNNNNNNNN
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
