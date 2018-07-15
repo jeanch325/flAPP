efd24356b8bdd11a27d8050d4a2a3633015ce0af
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-15 15:09:59 -0500

    made different sized bricks, they go at the bottom

--- flAPP/GameScene.swift
+++ flAPP/GameScene.swift
@@ -11,28 +11,44 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var brick = SKSpriteNode()
+    var bottomBrick = SKSpriteNode()
+    var topBrick = SKSpriteNode()
     var ball = SKShapeNode()
+    let wait = SKAction.wait(forDuration: 4.0) //change countdown speed here
+    var heightsArray = [100, 200, 300, 400]
+    var height = 300
+    
+    
+    
  
     
     
     override func didMove(to view: SKView) {
-        makeBrick()
         createBackground()
-         makeBall()
-        let moveBottomLeft = SKAction.move(to: CGPoint(x: frame.minX,y: frame.minY + 50), duration:2.0)
-        brick.run(moveBottomLeft)
-        
+        makeBall()
+        makeBottomBricks()
+        makeTopBrick()
     }
     
-    func makeBrick() {
-        brick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: 300))
-        brick.zPosition = 1
-        brick.position = CGPoint(x: frame.midX, y: frame.minY + 50)
-        brick.name = "brick"
-        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
-        brick.physicsBody?.isDynamic = false
-        addChild(brick)
+    func makeBottomBrick() {
+        bottomBrick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: height + 400))
+        bottomBrick.zPosition = 1
+        bottomBrick.position = CGPoint(x: frame.maxX + 50, y: frame.minY + 50)
+        bottomBrick.name = "bottomBrick"
+        bottomBrick.physicsBody = SKPhysicsBody(rectangleOf: bottomBrick.size)
+        bottomBrick.physicsBody?.isDynamic = false
+        bottomBrick.zPosition = 4
+        addChild(bottomBrick)
+    }
+    
+    func makeTopBrick() {
+        topBrick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        topBrick.zPosition = 1
+        topBrick.position = CGPoint(x: frame.midX, y: frame.maxY - 30)
+        topBrick.name = "topBrick"
+        topBrick.physicsBody = SKPhysicsBody(rectangleOf: bottomBrick.size)
+        topBrick.physicsBody?.isDynamic = false
+        addChild(topBrick)
     }
     
     func createBackground() {
@@ -72,9 +88,22 @@ class GameScene: SKScene {
         
     }
     
+    func makeBottomBricks(){
+        for i in 1...10{
+            height = heightsArray[i % 4]
+            print("height: \(height)")
+            makeBottomBrick()
+            let moveBottomLeft = SKAction.move(to: CGPoint(x: frame.minX - 50,y: frame.minY + 50), duration:4.0)
+            let wait1 = SKAction.wait(forDuration: 4.0*Double(i)) //change countdown speed here
+            let sequence = SKAction.sequence([wait1, moveBottomLeft])
+            bottomBrick.run(sequence)
+        }
+        
+        
+    }
     
     
-    
+
     
     
 }
