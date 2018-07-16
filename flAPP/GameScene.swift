ed083d58316fa0396a9d6cdf925bbfcf722e5b28
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-16 11:01:19 -0500

    put in zoe's new code for blocks

--- flAPP/GameScene.swift
+++ flAPP/GameScene.swift
@@ -9,84 +9,110 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
     
+    
+    var ball = SKShapeNode()
     var bottomBrick = SKSpriteNode()
     var topBrick = SKSpriteNode()
-    var ball = SKShapeNode()
     let wait = SKAction.wait(forDuration: 4.0) //change countdown speed here
     var heightsArray = [30, 40, 50, 60, 70]
     var height = 300
-    var center = 50
+    var centerTop = 50
+    var centerBottom = 100
     let displaySize: CGRect = UIScreen.main.bounds
+
     var startButton = SKLabelNode()
     
     
     override func didMove(to view: SKView) {
-        createBackground()
-         makeBall()
+        
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        //createBackground()
+        makeBall()
         createStartButton()
-        makeTopBrick()
-        makeBottomBrick()
-      
-        
-        
-        let moveBottomLeft = SKAction.move(to: CGPoint(x: frame.minX,y: frame.minY + 50), duration:2.0)
-        //l;brick.run(moveBottomLeft)
-        
-        
-        
+        moveBricks()
+       
     }
     
     func makeBottomBrick() {
-        let displayWidth = self.displaySize.width
         let displayHeight = self.displaySize.height
-        var heights = CGFloat(height*Int(displayWidth)/100)
-        bottomBrick = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: heights))
-        print(heights)
-        bottomBrick.zPosition = 1
-        center = height/2
-        bottomBrick.position = CGPoint(x: frame.maxX + 50, y: frame.minY + CGFloat(center))
+        var heightsBottom = CGFloat(height*Int(displayHeight)/100)
+        bottomBrick = SKSpriteNode(color: UIColor.red, size: CGSize(width: 100, height: heightsBottom))
+        //bottomBrick.zPosition = 1
+        centerBottom = Int(heightsBottom)/2
+        bottomBrick.position = CGPoint(x: frame.maxX + 50, y: frame.minY + CGFloat(centerBottom))
         bottomBrick.name = "bottomBrick"
         bottomBrick.physicsBody = SKPhysicsBody(rectangleOf: bottomBrick.size)
         bottomBrick.physicsBody?.isDynamic = false
-        bottomBrick.zPosition = 4
+        //bottomBrick.zPosition = 4
         addChild(bottomBrick)
     }
     
     func makeTopBrick() {
-        topBrick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
-        topBrick.zPosition = 1
-        topBrick.position = CGPoint(x: frame.midX, y: frame.maxY - 30)
+        let displayWidth = self.displaySize.width
+        let displayHeight = self.displaySize.height
+        var heightsTop = CGFloat((90 - height)*Int(displayHeight)/100)
+        topBrick = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: heightsTop))
+        //topBrick.zPosition = 1
+        centerTop = Int(heightsTop)/2
+        topBrick.position = CGPoint(x: frame.maxX + 50, y: frame.maxY - CGFloat(centerTop))
         topBrick.name = "topBrick"
         topBrick.physicsBody = SKPhysicsBody(rectangleOf: bottomBrick.size)
         topBrick.physicsBody?.isDynamic = false
+        //topBrick.zPosition = 4
         addChild(topBrick)
     }
     
-    func createBackground() {
-        let sky = SKTexture(imageNamed: "sky")
-        for i in 0...1 {
-            let skyBackground = SKSpriteNode(texture: sky)
-            skyBackground.zPosition = 0
-            skyBackground.size.height = frame.height
-            skyBackground.size.width = frame.width
-            skyBackground.position = CGPoint(x: skyBackground.size.width * CGFloat(i), y: 0)
-            addChild(skyBackground)
-            let moveLeft = SKAction.moveBy(x: -skyBackground.size.width, y: 0, duration: 20)
-            let moveReset = SKAction.moveBy(x: skyBackground.size.width, y: 0, duration: 0)
-            let moveLoop = SKAction.sequence([moveLeft, moveReset])
-            let moveForever = SKAction.repeatForever(moveLoop)
-            skyBackground.run(moveForever)
-            
+    func moveBricks(){
+        for i in 1...10{
+            let displayWidth = self.displaySize.width
+            let displayHeight = self.displaySize.height
+            var heightsBottom = CGFloat(height*Int(displayHeight)/100)
+            var heightsTop = CGFloat((30 - height)*Int(displayHeight)/100)
+            height = heightsArray[i % 5]
+            centerBottom = Int(heightsBottom)/2
+            centerTop = Int(heightsTop)/2
+            makeBottomBrick()
+            makeTopBrick()
+            let moveBottomLeft = SKAction.move(to: CGPoint(x: frame.minX - 50,y: frame.minY + CGFloat(centerBottom)), duration:4.0)
+            let wait1 = SKAction.wait(forDuration: 4.0*Double(i)) //change countdown speed here
+            let sequence = SKAction.sequence([wait1, moveBottomLeft])
+            bottomBrick.run(sequence)
+            let moveTopLeft = SKAction.move(to: CGPoint(x: frame.minX - 50,y: frame.maxY - CGFloat(centerTop)), duration:4.0)
+            let wait2 = SKAction.wait(forDuration: 4.0*Double(i)) //change countdown speed here
+            let otherSequence = SKAction.sequence([wait1, moveTopLeft])
+            topBrick.run(otherSequence)
         }
     }
+
+
+    
+//    func createBackground() {
+//        let sky = SKTexture(imageNamed: "sky")
+//        for i in 0...1 {
+//            let skyBackground = SKSpriteNode(texture: sky)
+//            skyBackground.zPosition = 0
+//            skyBackground.size.height = frame.height
+//            skyBackground.size.width = frame.width
+//            skyBackground.position = CGPoint(x: skyBackground.size.width * CGFloat(i), y: 0)
+//            addChild(skyBackground)
+//            let moveLeft = SKAction.moveBy(x: -skyBackground.size.width, y: 0, duration: 20)
+//            let moveReset = SKAction.moveBy(x: skyBackground.size.width, y: 0, duration: 0)
+//            let moveLoop = SKAction.sequence([moveLeft, moveReset])
+//            let moveForever = SKAction.repeatForever(moveLoop)
+//            skyBackground.run(moveForever)
+//            
+//        }
+//    }
         
         
     
     func makeBall() {
         ball = SKShapeNode(circleOfRadius: 10)
-        ball.zPosition = 1
+        //ball.zPosition = 1
         ball.position = CGPoint(x: frame.midX - 100, y: frame.midY)
         ball.strokeColor = .black
         ball.fillColor = .red
@@ -141,6 +167,21 @@ class GameScene: SKScene {
                 }
         
             }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("a: \(contact.bodyA.node?.name)")
+        print("b: \(contact.bodyB.node?.name)")
+        
+        if contact.bodyA.node?.name == "bottomBrick" || contact.bodyB.node?.name == "bottomBrick" || contact.bodyA.node?.name == "topBrick" || contact.bodyB.node?.name == "topBrick" {
+            //lives.text = "You lose"
+            print("You lose!")
+            ball.removeFromParent() //removes ball from game
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+               // self.restart()
+            }
+            
+        }
+    }
     
     
     
